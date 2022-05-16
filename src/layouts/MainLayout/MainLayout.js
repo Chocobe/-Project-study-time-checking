e6457240835966eaa2762a221900f2923884372b
김영우 <kyw05171@gmail.com>
2022-05-17 02:21:13 +0900

    MainLayout 컴포넌트 작성

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,15 +1,47 @@
 import React, {
-  //
+  useState,
+  useCallback,
 } from "react";
+import { Outlet } from "react-router-dom";
 
-const MainLayout = ({ children }) => {
+import ChocobeButton from "@/components/ChocobeButton/ChocobeButton";
+
+import "./MainLayout.scss";
+
+const MainLayout = () => {
+  // TODO: useCOntext() 필요
+  
+  const [buttonName] = useState("로그인");
+
+  const onClick = useCallback(() => console.log("로그인 버튼 클릭"), []);
+  
   return (
-    <div className="mainLayout">
-      <h1 className="mainLayout-title">
-        It's Main Layout Component
-      </h1>
+    <div className="MainLayout">
+      <header className="MainLayout-header">
+        <h1 className="MainLayout-header-title">
+          Study Time Checker
+        </h1>
 
-      {children}
+        <p className="MainLayout-header-description">
+          AI 와 함께 순수 공부시간을 측정해 보세요
+        </p>
+      </header>
+
+      <div className="MainLayout-actions">
+        <div className="MainLayout-actions-decorator" />
+
+        <ChocobeButton
+          onClick={onClick}
+          className="MainLayout-actions-button"
+          // fluid
+        >
+          {buttonName}
+        </ChocobeButton>
+      </div>
+
+      <main className="MainLayout-main">
+        <Outlet />
+      </main>
     </div>
   );
 };
