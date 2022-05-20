fe5ea7f934575dcc7755977dc956ee0e36ca1354
김영우 <kyw05171@gmail.com>
2022-05-20 22:28:04 +0900

    Home 페이지 컴포넌트 생성

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {
+  useContext,
+  useEffect,
+} from "react";
+
+import {
+  useNavigate,
+} from "react-router-dom";
+
+import {
+  MainStateContext,
+  MainDispatchContext,
+} from "@/context/MainContext"
 
 const Home = () => {
+  const state = useContext(MainStateContext);
+  const dispatch = useContext(MainDispatchContext);
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    dispatch.init();
+    const { email, password, token } = state;
+
+    if (email && password && token) {
+      navigator("/study");
+    }
+  }, []);
+  
   return (
     <div>
       <h1>Home Page</h1>
