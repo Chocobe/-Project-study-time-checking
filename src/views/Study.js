52df97ad37e876edb3b63ac16d61f3f7d0ae5455
김영우 <kyw05171@gmail.com>
2022-05-20 22:28:18 +0900

    Study 페이지 컴포넌트 생성

--- src/views/Study.js
+++ src/views/Study.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {
+  useContext,
+} from "react";
+
+import {
+  MainStateContext,
+} from "@/context/MainContext";
 
 const Study = () => {
+  const state = useContext(MainStateContext);
+  
   return (
     <div>
       <h1>Study Page</h1>
+
+      <div style={{
+        marginTop: "20px",
+        padding: "20px",
+        border: "1px solid #ff1493",
+      }}>
+        <h1>로그인 완료</h1>
+        <h1>Email: {state.email}</h1>
+        <h1>Password: {state.password}</h1>
+        <h1>Token: {state.token}</h1>
+      </div>
     </div>
   );
 };
