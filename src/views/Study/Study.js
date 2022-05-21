4696e67f1d9520db91e8d75bd8b2eb3b77079b8f
김영우 <kyw05171@gmail.com>
2022-05-21 20:26:44 +0900

    Study 페이지 컴포넌트 작성

--- src/views/Study/Study.js
+++ src/views/Study/Study.js
@@ -1,27 +1,66 @@
 import React, {
-  useContext,
+  // useContext,
+  useState,
+  useCallback,
+  useEffect,
 } from "react";
 
-import {
-  MainStateContext,
-} from "@/context/MainContext";
+import ChocobeLabeledImage from "@/components/LabeledImage/ChocobeLabeledImage";
+
+import "./Study.scss";
+
+// import {
+//   MainStateContext,
+// } from "@/context/MainContext";
 
 const Study = () => {
-  const state = useContext(MainStateContext);
+  // const state = useContext(MainStateContext);
+
+  const [mockSrc, setMockSrc] = useState();
+
+  const initMockCam = useCallback(async () => {
+    const { default: img } = await import("@/assets/imgs/cam-test-img.png");
+    setMockSrc(img);
+  }, []);
+
+  useEffect(() => {
+    initMockCam();
+  }, [])
   
   return (
-    <div>
-      <h1>Study Page</h1>
+    <div className="Study">
+      <figure className="Study-camWrapper">
+        <img
+          className="Study-camWrapper-cam"
+          src={mockSrc}
+          alt="캠 이미지"
+        />
+      </figure>
 
-      <div style={{
-        marginTop: "20px",
-        padding: "20px",
-        border: "1px solid #ff1493",
-      }}>
-        <h1>로그인 완료</h1>
-        <h1>Email: {state.email}</h1>
-        <h1>Password: {state.password}</h1>
-        <h1>Token: {state.token}</h1>
+      <div className="Study-record">
+        <ChocobeLabeledImage
+          fileName="study-removebg-preview.png"
+          alt="스터디 기록"
+          bgColor="#94D983"
+        >
+          00:10:10
+        </ChocobeLabeledImage>
+
+        <ChocobeLabeledImage
+          fileName="phone-removebg-preview.png"
+          alt="폰 사용 기록"
+          bgColor="#E7E997"
+        >
+          00: 02:30
+        </ChocobeLabeledImage>
+        
+        <ChocobeLabeledImage
+          fileName="empty-removebg-preview.png"
+          alt="자리비움 기록"
+          bgColor="#FFA8A8"
+        >
+          00:03:43
+        </ChocobeLabeledImage>
       </div>
     </div>
   );
