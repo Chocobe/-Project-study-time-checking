f30c62002500f22316f72825858f8dae895cf75b
김영우 <kyw05171@gmail.com>
2022-05-22 19:10:17 +0900

    Study 페이지 컴포넌트의 modalReducer 분리

---
+++ src/views/Study/modalReducer.js
@@ -0,0 +1,73 @@
+const modalReducer = (prevState, actions) => {
+  const {
+    type,
+    onOk,
+    onCancel,
+  } = actions;
+
+  switch (type) {
+    case "ADD": {
+      console.log("reducer - ADD() 호출");
+
+      return {
+        isOpen: true,
+        title: "Subject 등록",
+        okText: "등록",
+        cancelText: "취소",
+        okBgColor: "#03a9f4",
+        onOk,
+        onCancel,
+      };
+    }
+
+    case "EDIT": {
+      console.log("reducer - EDIT() 호출");
+      
+      return {
+        isOpen: true,
+        title: "Subject 수정",
+        okText: "수정",
+        cancelText: "취소",
+        okBgColor: "#03a9f4",
+        onOk,
+        onCancel,
+      }
+    }
+
+    case "DELETE": {
+      console.log("reducer - DELETE() 호출");
+
+      return {
+        isOpen: true,
+        title: "Subject 삭제",
+        description: 
+          <>
+            알고리즘 항목을 삭제 하시겠습니까? <br />
+            삭제 시, 복구할 수 없습니다.
+          </>,
+        okText: "삭제",
+        cancelText: "취소",
+        okBgColor: "#ff1493",
+        onOk,
+        onCancel,
+      };
+    }
+
+    case "CANCEL": {
+      const resultState = Object.keys(prevState).reduce((state, key) => ({
+        ...state,
+        [key]: undefined,
+      }), {});
+
+      resultState.isOpen = false;
+
+      return resultState;
+    }
+
+    default: {
+      console.log(`제공되지 않는 ${type} 입니다.`);
+    }
+  }
+}
+
+export default modalReducer;
