316b6bd9c7b0a0a33b7267498c925a6389a71e57
김영우 <kyw05171@gmail.com>
2022-05-22 16:52:45 +0900

    MainLayout template 구조 변경

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -35,8 +35,6 @@ const MainLayout = () => {
     await dispatch.login("초코비 이메일 👍", "초코비 비번 🎈");
 
     navigator("/study");
-    
-    console.log("222 번째로 호출 되야함");
   }, [dispatch, navigator]);
 
   const logout = useCallback(() => {
@@ -55,45 +53,47 @@ const MainLayout = () => {
 
   return (
     <div className="MainLayout">
-      <header className="MainLayout-header">
-        <h1 className="MainLayout-header-title">
-          Study Time Checker
-        </h1>
+      <div className="MainLayout-inner">
+        <header className="MainLayout-inner-header">
+          <h1 className="MainLayout-inner-header-title">
+            Study Time Checker
+          </h1>
 
-        <p className="MainLayout-header-description">
-          AI 와 함께 순수 공부시간을 측정해 보세요
-        </p>
-      </header>
+          <p className="MainLayout-inner-header-description">
+            AI 와 함께 순수 공부시간을 측정해 보세요
+          </p>
+        </header>
 
-      <div className="MainLayout-actions">
-        <div className="MainLayout-actions-decorator" />
+        <div className="MainLayout-inner-actions">
+          <div className="MainLayout-inner-actions-decorator" />
 
-        <ChocobeButton
-          className="MainLayout-actions-button"
-          onClick={onClickAuthButton}
-        >
-          {authButtonName}
-        </ChocobeButton>
+          <ChocobeButton
+            className="MainLayout-inner-actions-button"
+            onClick={onClickAuthButton}
+          >
+            {authButtonName}
+          </ChocobeButton>
 
-        {/* FIXME: OAuth2 Google 로그인 */}
-        <a href={process.env.REACT_APP_LOGIN_GOOGLE_URL} target="_black">로그인 테스트</a>
-      </div>
-
-      <main className="MainLayout-main">
-        <div className="MainLayout-main-intro">
-          <ul className="MainLayout-main-intro-list">
-            <li className="MainLayout-main-intro-list-item">
-              캠을 통해 AI 가 순수 공부시간을 측정해 줍니다.
-            </li>
-
-            <li className="MainLayout-main-intro-list-item">
-              AI 가 측정하는 시간은 3가지 형태로 측정합니다.
-            </li>
-          </ul>
+          {/* FIXME: OAuth2 Google 로그인 */}
+          <a href={process.env.REACT_APP_LOGIN_GOOGLE_URL} target="_black">로그인 테스트</a>
         </div>
 
-        <Outlet />
-      </main>
+        <main className="MainLayout-inner-main">
+          <div className="MainLayout-inner-main-intro">
+            <ul className="MainLayout-inner-main-intro-list">
+              <li className="MainLayout-inner-main-intro-list-item">
+                캠을 통해 AI 가 순수 공부시간을 측정해 줍니다.
+              </li>
+
+              <li className="MainLayout-inner-main-intro-list-item">
+                AI 가 측정하는 시간은 3가지 형태로 측정합니다.
+              </li>
+            </ul>
+          </div>
+
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
