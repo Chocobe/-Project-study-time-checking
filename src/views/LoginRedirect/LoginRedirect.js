c6a2fdc7e4f858e0870a5c89e934fd324cb2c841
김영우 <kyw05171@gmail.com>
2022-05-25 01:17:35 +0900

    LoginRedirect 컴포넌트 작성

---
+++ src/views/LoginRedirect/LoginRedirect.js
@@ -0,0 +1,28 @@
+import {
+  useContext,
+  useEffect,
+} from "react";
+
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+import { MainDispatchContext } from "@/context/MainContext";
+import { DISPATCH_TYPE } from "@/context/mainReducer";
+
+const LoginRedirect = () => {
+  const dispatch = useContext(MainDispatchContext);
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+    
+    dispatch({
+      type: DISPATCH_TYPE.LOGIN,
+      payload: { token },
+    });
+
+    window.close();
+  }, [dispatch, searchParams, navigate]);
+};
+
+export default LoginRedirect;
