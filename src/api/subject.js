0db019f1929c71d192e84e9baaccfeeef4a40fc7
김영우 <kyw05171@gmail.com>
2022-05-28 18:20:13 +0900

    subjects API PUT 추가

--- src/api/subject.js
+++ src/api/subject.js
@@ -8,6 +8,15 @@ export const subjectApi = {
   },
 
   POST({ subjectName }) {
+    console.log(`subjectApi 에서 받은 post subjectName: ${subjectName}`);
     return tokenApi.post(this.URI, { subjectName });
+  },
+
+  PUT({ subjectId, subjectName, timeRecord }) {
+    return tokenApi.put(`${this.URI}/${subjectId}`, {
+      subjectId,
+      subjectName,
+      timeRecord,
+    });
   },
 };
